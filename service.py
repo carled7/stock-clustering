7b6ef0b54d05d4be4b3a52947c7cc45042260578
carled7 <carlos_ed03@outlook.com>
2023-06-22 09:50:09 -0300

    Final

--- service.py
+++ service.py
@@ -4,8 +4,10 @@ import os
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
               
 def build_data_set(tickers_df):
     with open("out/output.csv", mode='w', newline='') as file:
@@ -61,8 +63,18 @@ def download_single_stock(ticker_name):
             print(f"Request failed for {ticker}: {response.status_code}")
 
 def train_kmeans_model(final_df):
-    X = final_df[['av_close', 'av_volume', 'av_dividend_amount', 'highest', 'lowest']]
-    kmeans = KMeans(n_clusters=2)  
+    
+    df = pd.read_csv('out/output.csv')
+
+    del df['ticker']
+
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(df)
+
+    scaled_df = pd.DataFrame(scaled_data, columns=df.columns)
+    
+    X = scaled_df[['av_close', 'av_volume', 'av_dividend_amount', 'highest', 'lowest']]
+    kmeans = KMeans(n_clusters=5)  
     kmeans.fit(X)
 
     return kmeans
@@ -88,15 +100,54 @@ def main(ticker_name):
                         cluster_new_stock['close'].max(),
                         ]])
     
-    plt.scatter(final_df['av_close'], final_df['av_volume'], c=kmeans.labels_, label='Stocks used to train')
-    plt.scatter(cluster_new_stock['close'].mean(), cluster_new_stock['volume'].mean(), color = 'red', label=(f'{ticker_name}'))
     
-    plt.legend(loc='upper right')
-    plt.title('Clustering Results')
+    custom_colors = ['#8B00FF','#00FFFF','#00FF00', '#FF7F00', '#FF0000']  
+
+    custom_cmap = mcolors.ListedColormap(custom_colors)
+    
+        
+    legend_elements = []
+    for label, color in zip(set(kmeans.labels_), ['#8B00FF','#00FFFF','#00FF00', '#FF7F00', '#FF0000']):
+        legend_elements.append(plt.Line2D([0], [0], marker='o', color='w', label=label,
+                                        markerfacecolor=color, markersize=10))
+        
+    plt.scatter(final_df['av_close'], final_df['av_volume'], c=kmeans.labels_, cmap=custom_cmap)
+    plt.scatter(cluster_new_stock['close'].mean(), cluster_new_stock['volume'].mean(), color = 'black', label=(f'{ticker_name}'))
+    
+    plt.legend(loc='upper right', handles=legend_elements)
+    
+    plt.title('av_volume x av_close')
     
     plt.xlabel('av_close',)
     plt.ylabel('av_volume')
     
     plt.savefig('static/chart.jpg', format='jpg')
+    plt.cla()
     
-    return 'yellow' if pred_cluster == 1 else 'purple'
+    plt.scatter(final_df['av_close'], final_df['highest'], c=kmeans.labels_, cmap=custom_cmap)
+    plt.scatter(cluster_new_stock['close'].mean(), cluster_new_stock['close'].max(), color = 'black', label=(f'{ticker_name}'))
+    
+    plt.legend(loc='upper right', handles=legend_elements)
+    
+    plt.title('highest x av_close')
+    
+    plt.xlabel('av_close',)
+    plt.ylabel('highest')
+    
+    plt.savefig('static/chart2.jpg', format='jpg')
+    plt.cla()
+    
+    plt.scatter(final_df['av_close'], final_df['lowest'], c=kmeans.labels_, cmap=custom_cmap)
+    plt.scatter(cluster_new_stock['close'].mean(), cluster_new_stock['close'].min(), color = 'black', label=(f'{ticker_name}'))
+    
+    plt.legend(loc='upper right', handles=legend_elements)
+    
+    plt.title('lowest x av_close')
+    
+    plt.xlabel('av_close',)
+    plt.ylabel('lowest')
+    
+    plt.savefig('static/chart3.jpg', format='jpg')
+    
+    
+    return pred_cluster
